80983cea21f9f864c4bf11b562c9a01aadaee944
Alexander Norwood <Alexander Norwood>
2017-02-09 00:23:19 -0800

    Finished Radar Graph

--- Drawing/TeamStats.java
+++ Drawing/TeamStats.java
@@ -99,57 +99,32 @@ Scanner lineScan = new Scanner(line);
 
 int id = lineScan.nextInt();
 
-int gear = lineScan.nextInt();
+double gear = lineScan.nextDouble();
 
-int floor =lineScan.nextInt();
+double floor =lineScan.nextDouble();
 
-int gtpc = lineScan.nextInt();
+double gtpc = lineScan.nextDouble();
 
-int score = lineScan.nextInt();
+double score = lineScan.nextDouble();
 
-int intake = lineScan.nextInt();
+double intake = lineScan.nextDouble();
 
-int target = lineScan.nextInt();
+double target = lineScan.nextDouble();
 
-int stpc = lineScan.nextInt();
+double stpc = lineScan.nextDouble();
 
-int move = lineScan.nextInt();
+double move = lineScan.nextDouble();
 
-int power = lineScan.nextInt();
+double power = lineScan.nextDouble();
 
-int hoppersize = lineScan.nextInt();
+double hoppersize = lineScan.nextDouble();
 
-int climb = lineScan.nextInt();
+double climb = lineScan.nextDouble();
 
 int speed1 = lineScan.nextInt();
 
 int speed2 = lineScan.nextInt();
-//Creates radar graph setup
-DrawingPanel panel = new DrawingPanel(200,200);
-Graphics g = panel.getGraphics();
-g.drawLine(25,100,175,100); //25,100 center left/right
-g.drawLine(135,35,100,100);
-g.drawLine(100-35,35,100,100);
-g.drawLine(100-35, 200-35, 100,100);
-g.drawLine(135, 200-35, 100,100);
-g.drawLine(25,100,135,35); //Cord 1
-g.drawString("Gears",30,35);
-g.drawLine(135,35,65,35); //Cord 2
-g.drawString("Cord2",0,90);
-g.drawLine(65,35,65,165); //Cord 3
-g.drawString("Test",100,100);
-g.drawLine(50,150,100,175); //Cord 4
-g.drawString("Test",100,100);
-g.drawLine(100,175,150,150); //Cord 5
-g.drawString("Test",100,100);
-g.drawLine(150,150,175,100); //Cord 6
-g.drawString("Test",100,100);
-//Input for Graph
-g.setColor(Color.YELLOW);
-int[] f = {50,100,100};
-int[] d = {50,25,100};
-g.fillPolygon(f,d,3);
-//Graph Input Ends
+
 System.out.println("");
 System.out.println("Stats for FRC team: " + id);
 System.out.println("");
@@ -168,6 +143,74 @@ System.out.println("Maneuverability: " + move + "%");
 System.out.println("Power:           " + power + "%\n");
 System.out.println("Climb Status: " + climb);
 System.out.println("Robot Speed one: " + speed1 + "fps Robot Speed two: " + speed2 + "fps");
+//Input for Graph
+gear = gear/100;
+move = move/100;
+power = power/100;
+score = score/100;
+hoppersize = hoppersize/100;
+climb = climb/100;
+Double a1 = new Double(100-75*gear);
+Double a2 = new Double(100-35*move);
+Double a3 = new Double(100+35*power);
+Double a4 = new Double(100+75*score);
+Double a5 = new Double(100+35*hoppersize);
+Double a6 = new Double(100-35*climb);
+int b1 = 100;
+Double b2 = new Double(100-65*move);
+Double b3 = new Double(100-65*power);
+int b4 = 100;
+Double b5 = new Double(100+65*hoppersize);
+Double b6 = new Double(100+65*climb);
+
+DrawingPanel panel = new DrawingPanel(200,250);
+Graphics g = panel.getGraphics();
+g.setColor(Color.YELLOW);
+
+int[] ax1 = {a1.intValue(),a2.intValue(),100};
+int[] ay1 = {b1,b2.intValue(),100};
+g.fillPolygon(ax1,ay1,3);
+
+int[] ax2 = {a2.intValue(),a3.intValue(),100};
+int[] ay2 = {b2.intValue(),b3.intValue(),100};
+g.fillPolygon(ax2,ay2,3);
+
+int[] ax3 = {a3.intValue(),a4.intValue(),100};
+int[] ay3 = {b3.intValue(),b4,100};
+g.fillPolygon(ax3,ay3,3);
+
+int[] ax4 = {a4.intValue(),a5.intValue(),100};
+int[] ay4 = {b4,b5.intValue(),100};
+g.fillPolygon(ax4,ay4,3);
+
+int[] ax5 = {a5.intValue(),a6.intValue(),100};
+int[] ay5 = {b5.intValue(),b6.intValue(),100};
+g.fillPolygon(ax5,ay5,3);
+
+int[] ax6 = {a6.intValue(),a1.intValue(),100};
+int[] ay6 = {b6.intValue(),b1,100};
+g.fillPolygon(ax6,ay6,3);
+//Graph Input Ends
+//Creates radar graph setup
+g.setColor(Color.BLACK);
+g.drawLine(25,100,175,100); //25,100 center left/right
+g.drawLine(135,35,100,100);
+g.drawLine(100-35,35,100,100);
+g.drawLine(100-35, 200-35, 100,100);
+g.drawLine(135, 200-35, 100,100);
+g.drawLine(25,100,65,35); //Cord 1
+g.drawString("Gears",0,90);
+g.drawLine(65,35,135,35); //Cord 2
+g.drawString("Speed",40,30);
+g.drawLine(135,35,175,100); //Cord 3
+g.drawString("Power",120,30);
+g.drawLine(175,100,135,165); //Cord 4
+g.drawString("Fuel",170,90);
+g.drawLine(135,165,65,165); //Cord 5
+g.drawString("Storage",120,180);
+g.drawLine(65,165,25,100); //Cord 6
+g.drawString("Climb",40,180);
+//g.drawString(id, 150,220);
 }
    public static void mathybit(Scanner c,String main[][]){
    int gears1 = 0; //Number of allies able to do gears
